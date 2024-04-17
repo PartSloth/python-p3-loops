e34142ca138429aa64723f34caa7500cf87b8aa8
Louiza Mak <louiza.mak@gmail.com>
2024-04-17 15:41:47 -0500

    complete lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i >= 1:
+        print(f'{i}')
+        i -= 1
+    print("Happy New Year!")
+        
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    int_list = [int * int for int in int_list]
+    return int_list
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    i = 1
+    for i in range(1,101):
+        if(i % 5 == 0 and i % 3 == 0):
+            print("FizzBuzz")
+        elif(i % 3 == 0):
+            print("Fizz")
+        elif(i % 5 == 0):
+            print("Buzz")
+        else: print(f'{i}')
